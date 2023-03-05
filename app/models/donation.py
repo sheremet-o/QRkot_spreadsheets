457adb1019aad396a87f1ebc8b8f378463f1ddce
Oksana Sheremet <sheremet5631@gmail.com>
2023-03-05 14:55:49 +0300

    added cat charity files

---
+++ app/models/donation.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, Text
+
+from app.core.db import PreBaseDonationCharity
+
+
+class Donation(PreBaseDonationCharity):
+    user_id = Column(Integer, ForeignKey(
+        'user.id', name='fk_donation_user_id_user'
+    ))
+    comment = Column(Text)
+
+    def __repr__(self) -> str:
+        return self.user_id
