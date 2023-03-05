b406853a7aefda9244c4a240616b4f26814b10bf
Oksana Sheremet <sheremet5631@gmail.com>
2023-03-05 15:33:56 +0300

    Готово к проверке

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import get_async_session
 from app.core.google_client import get_service
 from app.core.user import current_superuser
-from app.crud.charity_project import project_crud
+from app.crud.charity_project import charityproject_crud
 from app.services.google_api import (set_user_permissions, spreadsheets_create,
                                      spreadsheets_update_value)
 
@@ -20,7 +20,7 @@ async def get_report(
         wrapper_services: Aiogoogle = Depends(get_service)):
     '''Только для суперюзеров.\n
     Создаёт отчет о скорости закрытия проектя в гугл таблице.'''
-    projects = await project_crud.get_projects_by_completion_rate(session)
+    projects = await charityproject_crud.get_projects_by_completion_rate(session)
     spreadsheetid = await spreadsheets_create(wrapper_services)
     await set_user_permissions(spreadsheetid, wrapper_services)
     await spreadsheets_update_value(spreadsheetid, projects, wrapper_services)
--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-
-from app.core.init_db import create_first_superuser
 from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
 from app.core.init_db import create_first_superuser
+
 
 app = FastAPI(
     title=settings.app_title,
